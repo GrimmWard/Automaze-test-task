49f0c85ad04c3133f4f1bf9a83a9f04aaa9b0117
GrimmWard <vanakramar76@gmail.com>
2025-08-26 17:19:04 +0300

    delete log

--- frontend/api/config.ts
+++ frontend/api/config.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
-
-
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api",
 });
 
 export default api;
